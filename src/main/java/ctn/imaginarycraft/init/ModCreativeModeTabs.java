4081588d9aec51a3b53f4d7083fef0d1535989df
王小尽 <1809315974@qq.com>
2026-02-25 20:08:29 +0800

    更新翻译

--- src/main/java/ctn/imaginarycraft/init/ModCreativeModeTabs.java
+++ src/main/java/ctn/imaginarycraft/init/ModCreativeModeTabs.java
@@ -21,15 +21,15 @@ public final class ModCreativeModeTabs extends CreativeModeTabRegisterUtil {
   public static final DeferredRegister<CreativeModeTab> REGISTRY = ImaginaryCraft.modRegister(BuiltInRegistries.CREATIVE_MODE_TAB);
 
   public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EGO_CURIOS = register(
-    "ego_curios", "E.G.O 饰品", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
+    "ego_curios", "异想工艺|E.G.O饰品", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
       addRegistryItem(EgoCurioItems.REGISTRY, output), () ->
       EgoCurioItems.BENEDICTION.get().getDefaultInstance()));
   public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EGO_WEAPON = register(
-    "ego_weapon", "E.G.O 武器", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
+    "ego_weapon", "异想工艺|E.G.O武器", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
       addRegistryItem(EgoWeaponItems.REGISTRY, output), () ->
       EgoWeaponItems.IN_THE_NAME_OF_LOVE_AND_HATE.get().getDefaultInstance()));
   public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EGO_ARMOR = register(
-    "ego_armor", "E.G.O 护甲", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
+    "ego_armor", "异想工艺|E.G.O护甲", (name, zhCn) -> createCreativeModeTab(name, zhCn, (parameters, output) ->
       addRegistryItem(EgoArmorItems.REGISTRY, output), () ->
       EgoArmorItems.IN_THE_NAME_OF_LOVE_AND_HATE.chestplate().get().getDefaultInstance()));
   public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ITEMS = register(
