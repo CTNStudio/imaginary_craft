fbf6dd392c3c2345e602124b00d13d0429450bb0
王小尽 <1809315974@qq.com>
2026-03-12 14:51:37 +0800

    移除修复史诗战斗的LocalPlayerPatchMixin的不兼容的命中结果类型的修复方法

--- src/main/java/ctn/imaginarycraft/mixin/client/world/LocalPlayerPatchMixin.java
+++ src/main/java/ctn/imaginarycraft/mixin/client/world/LocalPlayerPatchMixin.java
@@ -1,22 +1,9 @@
 package ctn.imaginarycraft.mixin.client.world;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.*;
-import com.llamalad7.mixinextras.sugar.*;
-import net.minecraft.world.phys.*;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
 import yesman.epicfight.client.world.capabilites.entitypatch.player.*;
 
 @Mixin(LocalPlayerPatch.class)
 public abstract class LocalPlayerPatchMixin {
 
-  @WrapOperation(method = "canPlayAttackAnimation", at = @At(value = "INVOKE", target = "Lyesman/epicfight/client/events/engine/RenderEngine;hitResultEquals(Lnet/minecraft/world/phys/HitResult;Lnet/minecraft/world/phys/HitResult$Type;)Z", ordinal = 0))
-  private boolean imaginarycraft$canPlayAttackAnimation(
-    HitResult hitResult,
-    HitResult.Type hitType,
-    Operation<Boolean> original,
-    @Local(name = "hitResult") HitResult localHitResult
-  ) {
-    return original.call(localHitResult, hitType);
-  }
 }
